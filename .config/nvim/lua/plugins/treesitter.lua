135b48f11ab5966389af13f2ba45b6447e1a529d
Erick Lievana Poy <ericklievana@gmail.com>
2024-02-16 03:07:50 -0600

    Add autolist plugin. Add more filetypes to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,26 @@
 return{
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function () 
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { 
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "cpp",
-        "bash",
-        "html",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+		configs.setup({
+			ensure_installed = { 
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"cpp",
+				"bash",
+				"html",
+				"gitcommit",
+				"make",
+				"markdown",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
 
