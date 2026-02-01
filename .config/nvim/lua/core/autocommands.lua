67110730ca8bfe68e1d61c34a31125c1452c03bb
Erick Lievana Poy <ericklievana@gmail.com>
2026-01-31 21:28:59 -0600

    Fix config of treesitter.Remove autocommands plugin

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -14,28 +14,31 @@ vim.api.nvim_create_autocmd(
   }
 )
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'*'},
-  callback = function()
-    local filetype = vim.filetype.match({buf = vim.api.nvim_get_current_buf()})
-    local treesitter = require('nvim-treesitter')
-    local installed = treesitter.get_installed()
-    for _, language in ipairs(installed) do
-      if filetype == language then
-        vim.treesitter.start()
-        return
-      end
-    end
-    local available = treesitter.get_available()
-    for _, language in ipairs(available) do
-      if filetype == language then
-        print('Installing parser for '..filetype)
-        treesitter.install({language},{generate = true}):wait(60000)
-        print('Parser installed')
-        vim.treesitter.start()
-        return
-      end
-    end
-    print('Parser for '..filetype..' not found')
-  end,
-})
+
+vim.api.nvim_create_autocmd(
+  {'Filetype'},
+  {
+    pattern = {
+      'sh',
+      'c',
+      'cpp',
+      'diff',
+      'dockerfile',
+      'gitcommit',
+      'go',
+      'html',
+      'json',
+      'latex',
+      'lua',
+      'make',
+      'markdown',
+      'python',
+      'vim',
+      'vimdoc',
+      'xresources',
+      'yaml',
+      'zathurarc',
+    },
+    callback = function() vim.treesitter.start() end,
+  }
+)
