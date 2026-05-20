03bc37d1cdfc40c3b8cf5f7e9bca63135b6462c9
Erick Lievana Poy <ericklievana@gmail.com>
2026-05-20 15:35:28 -0600

    Add ftfile for typst

---
+++ .config/nvim/after/ftplugin/typst.lua
@@ -0,0 +1,8 @@
+-- Open zathura to view pdf
+vim.keymap.set(
+    'n',
+    '<LocalLeader>op',
+    function()
+        vim.system({'zathura', vim.fn.expand('%:p:r')..'.pdf'}, { text = true })
+    end
+)
