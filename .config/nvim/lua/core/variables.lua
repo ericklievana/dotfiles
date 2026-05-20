df5bdd2fa571595f9611c9eee977efe085f5d8ab
Erick Lievana Poy <ericklievana@gmail.com>
2026-05-20 16:15:52 -0600

    FIX unnecesary call to lazy.lua
    Also DELETE two deprecated calls for markdown configuration

--- .config/nvim/lua/core/variables.lua
+++ .config/nvim/lua/core/variables.lua
@@ -23,14 +23,8 @@ vim.g.markdown_folding = 1
 -- Enable highlight in long regions
 vim.g.mardown_minlines = 100
 
--- Enable fended coded block syntax highlight
-vim.g.markdown_fended_languages = ['python', 'bash=sh', 'c']
-
 -- Detect Markdown as Pandoc
 vim.g.markdown_md = 'pandoc'
-
--- Enable fended coded block syntax highlight
-vim.g['pandoc#syntax#codeblocks#embeds#langs'] = ['python', 'bash=sh', 'c']
 
 -- TeX language
 -- Set default to latex
